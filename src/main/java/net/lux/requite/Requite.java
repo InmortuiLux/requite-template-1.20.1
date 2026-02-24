ebf9a8111879ad1b68c25952fda661e73558b601
Ilyas Reijnders <ilyasreijnders@Ilyass-MacBook-Pro.local>
2026-02-24 17:35:01 +0800

    Initial commit

---
+++ src/main/java/net/lux/requite/Requite.java
@@ -0,0 +1,18 @@
+package net.lux.requite;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Requite implements ModInitializer {
+	public static final String MOD_ID = "requite";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/lux/requite/RequiteClient.java
@@ -0,0 +1,9 @@
+package net.lux.requite;
+import net.fabricmc.api.ClientModInitializer;
+public class RequiteClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
+
